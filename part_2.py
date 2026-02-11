b3c883496b9ba4e60cc18522fbc8fe6d93c976ea
HRB05 <hrb10@kent.ac.uk>
2026-02-11 11:36:56 +0000

    2.3 Finished

--- part_2.py
+++ part_2.py
@@ -6,3 +6,10 @@ rover_status = {
 }
 print(rover_status["Battery"])
 print(rover_status)
+
+mission_log = [{"Site": "Crater A", "Radiation": "Low", "Water": False},{"Site": "Dune B", "Radiation": "High", "Water": True}]
+for i in range(len(mission_log)):
+    sites = mission_log[i]["Site"]
+    rads = mission_log[i]["Radiation"]
+    print(f"Site {sites} has {rads.lower()} radiation")
+    i += 1
