cf230f0c05e2456a4cde64b4312d3162f124a0e2
HRB05 <hrb10@kent.ac.uk>
2026-02-11 12:16:59 +0000

    4.1

--- part_4.py
+++ part_4.py
@@ -1,1 +1,15 @@
+travel_log = []
+while True:
+    try:
+        x = input("Sensor Reading (Slope Angle): ")
+        int(x)
+        if x > 45:
+            print("CRITICAL TILT! HALTING.")
+            break
+        else:
+            travel_log.append(x)
+            print("Path Stable. Moving forward.")
+    except:
+        print("Sensor Glitch")
+        continue
 
