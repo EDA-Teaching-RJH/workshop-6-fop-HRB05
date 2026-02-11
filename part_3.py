147f32e32a40eddd6c1015720ef434b25b3bd06e
HRB05 <hrb10@kent.ac.uk>
2026-02-11 12:09:44 +0000

    3.3 Finished

--- part_3.py
+++ part_3.py
@@ -3,14 +3,15 @@ def get_coordinate():
     while True:
         try:
             x = input("What is the X coordinate: ")
-            if -100 < x < 100:
-                print(f"Coordinate set to {int(x)}")
-                break
-            else:
-                print("Coordinate out of range")
-                continue
+            print(f"Coordinate set to {int(x)}")
         except:
             print("Invalid coordinate")
+            continue
+        if -100 < x < 100:
+            print(f"Coordinate set to {int(x)}")
+            break
+        else:
+            print("Coordinate out of range")
             continue
 
 get_coordinate()
